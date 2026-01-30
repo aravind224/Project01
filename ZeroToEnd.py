ac4853fd0c0813031102a3e4b688947d71de6f68
aravind224 <v.aravind224@gmail.com>
2026-01-30 23:58:52 +0530

    Updation of ZeroToEnd Code

--- ZeroToEnd.py
+++ ZeroToEnd.py
@@ -1,19 +1,23 @@
-def MoveZerosToEnd(arr,n):
-    j=-1
-    for i in range(0,n):
-        if arr[i]==0:
-            j=i
+def MoveZerosToEnd(arr, n):
+    j = -1
+    for i in range(0, n):
+        if arr[i] == 0:
+            j = i
             break
 
-    for i in range(j+1,n):
-        if arr[i]!=0:
-            arr[i],arr[j]= arr[j],arr[i]
-            j+=1
+    if j == -1:
+        return arr
+
+    for i in range(j + 1, n):
+        if arr[i] != 0:
+            arr[i], arr[j] = arr[j], arr[i]
+            j += 1
 
     return arr
 
-arr= [1,2,0,4,0,5,0,3,5,6,4,0,0]
-n=13
 
-print("Values:",MoveZerosToEnd(arr,n))
-        
+# This part runs ONLY when you manually run the file
+if __name__ == "__main__":
+    arr = [1, 2, 0, 4, 0, 5, 0, 3, 5, 6, 4, 0, 0]
+    n = len(arr)
+    print("Values:", MoveZerosToEnd(arr, n))
